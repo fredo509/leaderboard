473c801dff22e7f4b0c9b4e714ff11e529381b87
Fredo St Fleur <fredostfleur23@gmail.com>
2023-04-11 14:59:43 -0400

    - Display body with javascript - Use module to import show body functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import './style.css';
+import showBody from './modules/showBody.js';
 
-const template = document.getElementById('template');
-const showBody = () => {
-
-};
+// const template = document.getElementById('template');
 
 document.body.onload = () => {
   showBody();
