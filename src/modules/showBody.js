e1020ea35db88b79196a89a946094a539a3b1d87
Fredo St Fleur <fredostfleur23@gmail.com>
2023-04-11 16:14:55 -0400

    Try to implement Add player functionality

--- src/modules/showBody.js
+++ src/modules/showBody.js
@@ -5,20 +5,21 @@ const showBody = () => {
       <section>
         <div id="scoreBlock">
           <div id="scorehead">
-            <p class="title">Recent scores</p>
-            <button id="refresh" type="button"> Refresh</button>
+             <p class="title">Recent scores</p>
+             <button id="refresh" type="button"> Refresh</button>
           </div>
-
+            <div id="template">
+             
+            </div>
         </div>
-        <div id="template">
-        </div>
+       
         <div id="add">
           <p class="title">Add your score</p>
           <div class="in">
             <form action="">
               <input type="text" placeholder="Your name" id="name">
               <input type="number" placeholder="Your score" id="score">
-              <button type="submit" id="btn-submit">Submit</button>
+              <button type="submit" id="btsubmit"> Submit </button>
             </form>
           </div>
         </div>
